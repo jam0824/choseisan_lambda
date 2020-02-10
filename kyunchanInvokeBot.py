70b5206246fd87184f4f8ae25cea50e598dac9c5
m <aku_soku_zan_1bantai@hotmail.com>
2020-02-11 02:04:58 +0900

    リトライ回避

--- kyunchanInvokeBot.py
+++ kyunchanInvokeBot.py
@@ -1,23 +1,27 @@
 import json
 import boto3
+import os
 import logging
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-def lambda_handler(event: dict, context):
+def lambda_handler(event, context):
 
     logging.info(json.dumps(event))
+
+    
     if "challenge" in event:
         return event.get("challenge")
-
-    client = boto3.client('lambda')
-    response = client.invoke(
-        FunctionName='kyunchan',
-        InvocationType='Event',
-        LogType='Tail',
-        Payload= json.dumps(event)
-    )
+    
+    if "X-Slack-Retry-Num" not in event["headers"]:
+        client = boto3.client('lambda')
+        response = client.invoke(
+            FunctionName='kyunchan',
+            InvocationType='Event',
+            LogType='Tail',
+            Payload= json.dumps(event)
+        )
 
     return {
         'statusCode': 200,
---
+++ test.py
@@ -0,0 +1,5 @@
+print("hello")
+event = {"headers":"hello"}
+print(event["headers"] )
+if "test" not in event["headers"]:
+	print("none")
