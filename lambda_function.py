a10f5bc9e4a15481d77a48949c9d5edc68dc3c21
m <aku_soku_zan_1bantai@hotmail.com>
2020-02-10 21:50:12 +0900

    return kyunchan OK

--- lambda_function.py
+++ lambda_function.py
@@ -31,7 +31,7 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('kyunchan OK!')
     }
     
 def selector(command):
